c2c02056116d7a1b86961aef1c7ca26efd78fa66
Matt Gadda <mgadda@gmail.com>
2015-01-12 22:26:47 -0800

    init stack

--- loader.s
+++ loader.s
@@ -16,3 +16,11 @@ loader:
   mov eax, 0xCAFEBABE
 .loop:
   jmp .loop
+
+KERNEL_STACK_SIZE equ 4096
+
+section .bss
+align 4
+kernel_stack:
+  resb KERNEL_STACK_SIZE
+  mov esp, kernel_stack + KERNEL_STACK_SIZE
