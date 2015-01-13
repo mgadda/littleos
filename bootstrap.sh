0706d904b4aa10c533ce48111c27df7f13a43ca0
Matt Gadda <mgadda@gmail.com>
2015-01-13 08:05:47 -0800

    install git-all package during box provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 apt-get update
-apt-get install -y build-essential nasm genisoimage bochs bochs-sdl
+apt-get install -y build-essential nasm genisoimage bochs bochs-sdl git-all
 echo "display_library: sdl" >> ~/.bochsrc
