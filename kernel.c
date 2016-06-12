433a7f83043bc16f157ee768c0bba2486b2ac48b
Matt Gadda <mgadda@gmail.com>
2016-06-12 16:25:15 -0700

    also verify irq4 is set up correctly

--- kernel.c
+++ kernel.c
@@ -12,10 +12,16 @@
 #include "isr.h"
 
 bool int3_happened = false;
+bool int4_happened = false;
 
 void int3_handler(registers_t regs) {
   debug("interrupt 3 detected.");
   int3_happened = true;
+}
+
+void int4_handler(registers_t regs) {
+  debug("interrupt 4 detected.");
+  int4_happened = true;
 }
 
 void kernel(multiboot_info_t *info) {
@@ -31,9 +37,11 @@ void kernel(multiboot_info_t *info) {
 
   debug("Generating random interrupts...");
   register_interrupt_handler(3, int3_handler);
+  register_interrupt_handler(4, int4_handler);
   asm volatile ("int $0x3");
   asm volatile ("int $0x4");
   if (!int3_happened) PANIC("interrupts improperly configured, no point in continuing.");
+  if (!int4_happened) PANIC("interrupts improperly configured, no point in continuing.");
 
   //init_timer(19);
   init_keyboard();
